86a8a39c3b33782eb1d4a888ed0001fc46bb25d9
Tuananh28597 <86956980+Tuananh28597@users.noreply.github.com>
2021-07-21 14:50:08 +0700

    update ham1,2

---
+++ product.js
@@ -0,0 +1,7 @@
+let products=["iphone 5","Iphone 6","Iphone 3"];
+showAll()
+function showAll(){
+
+     lkjhkljlkjlkjlkjlkjrtsdfgdhgdfhgf
+    document.getElementById("products").innerText="Danh sach sp day"
+}
